92a9c8ac3c70ad966f57aaf4bae559c584db927d
so_young <qksthdhd94@gmail.com>
2019-11-19 19:43:23 +0900

    Initial commit

---
+++ app/src/main/java/com/example/a20191119_01_banklistpractice/BaseActivity.kt
@@ -0,0 +1,12 @@
+package com.example.a20191119_01_banklistpractice
+
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity : AppCompatActivity(){
+
+    var mContext = this;
+    abstract fun setupEvents()
+    abstract fun setValues()
+
+
+}
